ebf7956beb343541faf53a04609d9027d1768423
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-17 15:50:36 -0400

    working for dimes

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,34 +1,38 @@
-# def get_change(change_value)
-# 	change_hash = {}
-# 	amount_remaining = change_value
-# 	if change_value >= 10 && change_value <= 24
-# 		change_hash["dime"] = change_value % 10
-		
-# 	end 
-# 	if change_value >= 5 && change_value <= 9
-# 		change_hash["nickel"] = 1
-# 		change_value = change_value - 5
-# 	end
-# 	if change_value < 5 && change_value > 0
-# 		change_hash["penny"] = change_value
-# 		change_value = change_value - change_hash[:penny].to_i * 1
-# 	end
-# 	change_hash
-# end
-
 def get_change(change_value)
 	change_hash = {}
-	until change_value == 0
-	if change_value >= 10 && change_value <= 24
-		change_hash["dime"] = change_value % 10
-		change_value = change_value - (change_hash["dime"].to_i * 10)
-	elsif change_value >= 5 && change_value <= 9
+	# amount_remaining = change_value
+	if change_value >= 10 
+		change_hash["dime"] = change_value / 10
+		change_value = change_value % 10
+		
+	end 
+	if change_value >= 5
 		change_hash["nickel"] = 1
 		change_value = change_value - 5
-	elsif change_value < 5 && change_value > 0
-		change_hash["penny"] = change_value
-		change_value = change_value - change_hash[:penny].to_i * 1
 	end
+	if change_value >= 1
+		change_hash["penny"] = change_value
+		
 	end
 	change_hash
 end
+
+# def get_change(change_value)
+# 	change_hash = {}
+# 	until change_value == 0
+# 	if change_value >= 25 && change_value <= 99
+# 		change_hash["quarter"] = change_value % 25
+# 		change_value = change_value - (change_hash["quarter"] * 10)
+# 	if	change_value >= 10 && change_value <= 24
+# 		change_hash["dime"] = change_value % 10
+# 		change_value = change_value - (change_hash["dime"]* 10)
+# 	elsif change_value >= 5 && change_value <= 9
+# 		change_hash["nickel"] = 1
+# 		change_value = change_value - 5
+# 	elsif change_value < 5 && change_value > 0
+# 		change_hash["penny"] = change_value
+# 		change_value = change_value - change_hash[:penny] * 1
+# 	end
+# 	end
+# 	change_hash
+# end
